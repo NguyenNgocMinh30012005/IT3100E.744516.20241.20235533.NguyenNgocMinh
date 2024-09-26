46795f71d806f15f60bf36f2a043f605768dd843
Nguyen Ngoc Minh <nguyenngocminh2005bn@gmail.com>
2024-09-26 20:38:57 +0700

    message

--- MATHoperations.java
+++ MATHoperations.java
@@ -10,9 +10,9 @@ public class MATHoperations {
         double num1 = Double.parseDouble(strNum1);
         double num2 = Double.parseDouble(strNum2);
 
-        sum = num1 + num2;
-        difference = abs(num1 - num2);
-        quotient = num1 / num2;
+        double sum = num1 + num2;
+        double difference = Math.abs(num1 - num2);
+        double quotient = num1 / num2;
 
         JOptionPane.showMessageDialog(null, sum, "The sum of two double numbers",JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, difference, "The difference of two double numbers",JOptionPane.INFORMATION_MESSAGE);
