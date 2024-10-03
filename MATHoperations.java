ef2adfe69b9f6e452c54dcde172f7a631443f585
Nguyen Ngoc Minh <nguyenngocminh2005bn@gmail.com>
2024-10-03 20:44:39 +0700

    message

--- MATHoperations.java
+++ MATHoperations.java
@@ -2,7 +2,6 @@ import javax.swing.JOptionPane;
 public class MATHoperations {
     public static void main(String[] args){
         String strNum1, strNum2;
-        String strNotification = "You've just entered: ";
 
         strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ","Input the first number", JOptionPane.INFORMATION_MESSAGE);
         strNum2 = JOptionPane.showInputDialog(null, "Please input the first number: ","Input the first number", JOptionPane.INFORMATION_MESSAGE);
